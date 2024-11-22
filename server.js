b309aa550fa44a82e048207be4cddef0290f65f9
Mateus Rodrigues <seuemail@email.com>
2024-11-22 18:47:27 -0300

    Criando conexão com o banco de dados

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 import express from "express";
+import conectarAoBanco from "./src/config/dbconfig.js"; 
+const conexao = await conectarAoBanco(process.env.STRING_CONEXAO);//Quando essa função conectar ao BD todos os dados relativos à ela vão ficar guardados na variável 'conexao'
 
 const posts = [
     {
@@ -18,14 +20,21 @@ const posts = [
     } //Dados hospedados localmente, array em memória
 ];//Temos uma lista '[]' e dentro dessa lista temos um objeto '{}', composto por um conjunto de chaves (descricao, imagem) e valor ("Uma foto teste","https")
 
-const app = express(); //express() é uma função
+const app = express();
 app.use(express.json());//Indica que a nossa aplicação tem funcionalidade de converter uma estrutura em json
 
 app.listen(3000, () => { //3000 é uma porta padrão de servidor local
     console.log("Servidor escutando..."); //O servidor escuta a porta 3000 e a função nesse momento é exibir a mensagem
 });//O Servidor está ouvindo requisições que chegam na porta 3000
 
-app.get("/posts", (req, res) => { //A rota '/posts' do tipo 'get' vai pegar algo do servidor após uma requisição (req) e responder (res)
+async function getTodosPosts() {//'async' transforma a função em assincrona, sem sincronia
+    const db = conexao.db("imersao-instabytes")//Objeto db representa o banco de dados chama 'imersao-instabytes'
+    const colecao = db.collection("posts")//'db.collection' foi no banco e abriu uma coleção específica (posts)
+    return colecao.find().toArray()
+};
+
+app.get("/posts", async (req, res) => { //A rota '/posts' do tipo 'get' vai pegar algo do servidor após uma requisição (req) e responder (res)
+    const posts = await getTodosPosts()//'await' usada dentro de função assincrona (async), indica a conclusão da operação antes de continuar a execução
     res.status(200).json(posts);//.json sabe que tem que converter a resposta, em string, que ele recebe em um formato que o js consiga trabalhar
 });
 
